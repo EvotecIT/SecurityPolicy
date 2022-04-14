52fd8bc82373db934184425f4a93d8e2d3074bd8
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-04-14 19:57:40 +0200

    Add All switch

--- Public/Get-UserRightsAssignment.ps1
+++ Public/Get-UserRightsAssignment.ps1
@@ -1,8 +1,34 @@
 ﻿function Get-UserRightsAssignement {
-    [cmdletBinding()]
+    <#
+    .SYNOPSIS
+    Provides a list of users assigned to a specific role.
+
+    .DESCRIPTION
+    Provides a list of users assigned to a specific role.
+
+    .PARAMETER UserRightsAssignment
+    Get all users for specific user rights assignment for specified computer.
+
+    .PARAMETER Computer
+    Parameter description
+
+    .PARAMETER All
+    Get all users for all user rights assignment for the specified computer.
+
+    .EXAMPLE
+    Get-UserRightsAssignement -All
+
+    .EXAMPLE
+    Get-UserRightsAssignement -UserRightsAssignment SeBackupPrivilege
+
+    .NOTES
+    General notes
+    #>
+    [cmdletBinding(DefaultParameterSetName = 'UserRights')]
     param(
-        [SecurityEditor.UserRightsAssignment] $UserRightsAssignment,
-        [alias('ComputerName')][string] $Computer
+        [parameter(Mandatory, ParameterSetName = 'UserRights')][SecurityEditor.UserRightsAssignment] $UserRightsAssignment,
+        [alias('ComputerName')][string] $Computer,
+        [parameter(ParameterSetName = 'All')][switch] $All
     )
 
     try {
@@ -20,15 +46,32 @@
             return
         }
     }
-    try {
-        $LsaWrapper.GetPrivileges($UserRightsAssignment)
-    } catch {
-        if ($PSBoundParameters.ErrorAction -eq 'Stop') {
-            Write-Error "Could not get privileges for $UserRightsAssignment. Error: $($_.Exception.Message)"
-            return
-        } else {
-            Write-Warning -Message "Get-UserRightsAssignement - Could not get privileges for $UserRightsAssignment. Error: $($_.Exception.Message)"
-            return
+    if ($All) {
+        $Output = [ordered] @{}
+        $EnumValues = [Enum]::GetNames([SecurityEditor.UserRightsAssignment])
+        foreach ($Value in $EnumValues | Sort-Object) {
+            $Output[$Value] = try {
+                $LsaWrapper.GetPrivileges($Value)
+            } catch {
+                if ($PSBoundParameters.ErrorAction -eq 'Stop') {
+                    Write-Error "Could not get privileges for $Value. Error: $($_.Exception.Message)"
+                    return
+                } else {
+                    Write-Warning -Message "Get-UserRightsAssignement - Could not get privileges for $Value. Error: $($_.Exception.Message)"
+                }
+            }
+        }
+        $Output
+    } else {
+        try {
+            $LsaWrapper.GetPrivileges($UserRightsAssignment)
+        } catch {
+            if ($PSBoundParameters.ErrorAction -eq 'Stop') {
+                Write-Error "Could not get privileges for $UserRightsAssignment. Error: $($_.Exception.Message)"
+                return
+            } else {
+                Write-Warning -Message "Get-UserRightsAssignement - Could not get privileges for $UserRightsAssignment. Error: $($_.Exception.Message)"
+            }
         }
     }
     try {
