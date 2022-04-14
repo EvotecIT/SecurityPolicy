2f0c015202d4717cadaacd755e3e55244c9f0569
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-04-14 17:35:09 +0200

    Example

---
+++ Examples/GetUserRights.ps1
@@ -0,0 +1,11 @@
+﻿Import-Module .\SecurityPolicy.psd1 -Force
+
+$Output = Get-UserRightsAssignement -UserRightsAssignment SeBackupPrivilege #-Computer AD1
+$Output | Format-Table
+
+Add-UserRightsAssignement -UserRightsAssignment SeBackupPrivilege -Identity "Evotec\Administrator"
+
+$Output = Get-UserRightsAssignement -UserRightsAssignment SeBackupPrivilege #-Computer AD1
+$Output | Format-Table
+
+Remove-UserRightsAssignement -UserRightsAssignment SeBackupPrivilege -Identity "Evotec\Administrator"
