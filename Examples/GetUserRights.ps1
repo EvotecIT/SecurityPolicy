82da58fab518934c7aaedabb6e4d31ddb5af5086
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-10-16 18:23:09 +0200

    Update example

--- Examples/GetUserRights.ps1
+++ Examples/GetUserRights.ps1
@@ -1,5 +1,13 @@
 ﻿Import-Module .\SecurityPolicy.psd1 -Force
 
+$Output = Get-UserRightsAssignment -UserRightsAssignment SeTrustedCredManAccessPrivilege #-Computer AD1
+$Output | Format-Table
+
+Remove-UserRightsAssignment -UserRightsAssignment SeTrustedCredManAccessPrivilege -Identity "S-1-5-21-853615985-2870445339-3163598659-4098"
+
+$Output = Get-UserRightsAssignment -UserRightsAssignment SeTrustedCredManAccessPrivilege #-Computer AD1
+$Output | Format-Table
+
 $Output = Get-UserRightsAssignment -UserRightsAssignment SeBackupPrivilege #-Computer AD1
 $Output | Format-Table
 
@@ -9,3 +17,6 @@ $Output = Get-UserRightsAssignment -UserRightsAssignment SeBackupPrivilege #-Com
 $Output | Format-Table
 
 Remove-UserRightsAssignment -UserRightsAssignment SeBackupPrivilege -Identity "Evotec\Administrator"
+
+$Output = Get-UserRightsAssignment -UserRightsAssignment SeBackupPrivilege #-Computer AD1
+$Output | Format-Table
